493f18671d38d2423c92fc4bf2f748889c3858f1
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2018-07-11 22:03:33 -0300

    Inserir e Alterar

--- index.php
+++ index.php
@@ -16,8 +16,21 @@ require_once("config.php");
 //echo json_encode($search);
 
 // Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("jose", "1234567890");
+//echo $usuario;
+
+// Criando um novo usuário
+//$aluno = new Usuario("aluno", "@lun0");
+//$aluno->insert();
+//echo $aluno;
+
 $usuario = new Usuario();
-$usuario->login("jose", "1234567890");
+
+$usuario->loadById(11);
+
+$usuario->update("professor", "!@#$");
+
 echo $usuario;
 
  ?>
