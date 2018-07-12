acfd41b85811bd61566aedf2cad92c1fbed69d4b
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2018-07-11 21:30:46 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 
 require_once("config.php");
 
-$user = new Usuario();
+// Carrega um usuário
+//$user = new Usuario();
+//$user->loadById(4);
+//echo $user;
 
-$user->loadById(4);
+// Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $user;
+// Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+// Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("jose", "1234567890");
+echo $usuario;
 
  ?>
